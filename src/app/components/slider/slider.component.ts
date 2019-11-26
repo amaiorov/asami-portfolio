695a391e54a13894c868e125174459932eac1ec3
Alexey Maiorov <alexey.maiorov@utc.com>
2019-11-26 11:22:54 -0500

    update carousel component

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -8,18 +8,33 @@ import { Component, OnInit, Input, HostListener } from '@angular/core';
 export class SliderComponent implements OnInit {
   @Input() slides: any[] = [];
 
+  activeSlide: number;
+  totalSlides: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.activeSlide = 0;
+    this.totalSlides = this.slides.length;
+  }
+
+  goto(slide) {
+    this.activeSlide = slide;
   }
 
   @HostListener('swipeleft', ['$event']) onSwipeLeft($event) {
-    alert('swipe left')
+    // alert('swipe left')
+    if (this.activeSlide > 0) {
+      this.goto(this.activeSlide - 1);
+    }
     // console.log($event)
   }
 
   @HostListener('swiperight', ['$event']) onSwipeRight($event) {
-    alert('swipe right')
+    // alert('swipe right')
+    if (this.activeSlide < this.totalSlides - 1) {
+      this.goto(this.activeSlide + 1);
+    }
     // console.log($event)
   }
 
