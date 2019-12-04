8dd514f6914fdfd6f57b56b3796d8d773371acab
Alexey Maiorov <alexey.maiorov@utc.com>
2019-12-04 16:27:36 -0500

    update mobile nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class NavComponent implements OnInit {
 
   // @Input() currentCategory;
+  @ViewChild('menu') menu: ElementRef;
+
   isMenuOpen = false;
 
   constructor() { }
@@ -18,6 +20,7 @@ export class NavComponent implements OnInit {
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
     document.body.classList.toggle('no-scroll');
+    // this.menu.nativeElement.classList.toggle('animate').toggle('animate');
   }
 
 }
